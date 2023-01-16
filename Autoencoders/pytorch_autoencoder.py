031524f25f7a49c4e9af8ad05580305c50401caa
DimitrisMitroulis <amfimitrul@gmail.com>
2023-01-16 17:52:08 +0200

    changes and new gan folder

--- Autoencoders/pytorch_autoencoder.py
+++ Autoencoders/pytorch_autoencoder.py
@@ -28,51 +28,77 @@ test_dataset = torchvision.datasets.MNIST(
 )
 
 train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=128, shuffle=True, num_workers=8, pin_memory=True
+    train_dataset, batch_size=256, shuffle=True, num_workers=0, pin_memory=True
 )
 
 test_loader = torch.utils.data.DataLoader(
-    test_dataset, batch_size=32, shuffle=False, num_workers=8
+    test_dataset, batch_size=32, shuffle=False, num_workers=0
+)
+
+example_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=1, shuffle=True, num_workers=0,drop_last=True,
+    )
+
+
+
+
+rbg_transform = transforms.ToTensor()
+
+stanford_train_dataset = torchvision.datasets.StanfordCars(
+    root="~/torch_datasets", transform=rbg_transform, download=True
 )
 
 
-example_loade = trorch.utils.data.DataLoader(
-    test_dataset, batch_size=1, shuffle=true, num_workers=4
-)
+
+
 
 # %% plot helper func
 
+#Helper functions
 def plot():
-    f, axarr = plt.subplots(2,2)
+    f, axarr = plt.subplots(2)
 
     for i, item in enumerate(image):
-
     # Reshape the array for plotting
         item = item.reshape(-1, 28, 28)
-        axarr[0,0].imshow(item[0].cpu())
+        axarr[0].imshow(item[0].cpu())
 
     for i, item in enumerate(reconstructed):
         item = item.reshape(-1, 28, 28).cpu()
         item = item.detach().numpy()
-        axarr[0,1].imshow(item[0])
-        
-def getImage():
-     for image, _ in example_loader:
-        reconstructed = model(image)
-        
-        f, axarr = plt.subplots(2)
-        item = item.reshape(-1, 28, 28)
-        axarr[0].imshow(item[0].cpu())
-        
-        item = item.reshape(-1, 28, 28).cpu()
-        item = item.detach().numpy()
         axarr[1].imshow(item[0])
         
+        
+        
+def showExample():
+    for image, _ in example_loader:
+        f, axarr = plt.subplots(2)
+        image = image.reshape(-1,28*28).to(device)
 
+        model.to(device)
+        recon = model(image)
+
+        image = image.reshape(-1, 28, 28)
+        axarr[0].imshow(image[0].cpu())
+
+
+        recon = recon.reshape(-1, 28, 28).to('cpu')
+        axarr[1].imshow(recon[0].detach().numpy())
+
+        break
+
+def add_noise(inputs,variance):
+    noise = torch.randn_like(inputs)
+    return inputs + variance*noise
+
+        
+        
+        
+        
 
 
 # %% autoencoder class 
-input_size = 28*28 #784
+input_size = 28*28  #784
 hidden_size = 128
 code_size = 32
 
@@ -106,6 +132,8 @@ class autoencoder(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
     
+        
+        
 # %% Define model, loss, optimzer
 
 # Model Initialization
